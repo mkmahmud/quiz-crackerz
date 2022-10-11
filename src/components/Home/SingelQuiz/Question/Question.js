310aa57e27191be400e1b8b62f5a845373d6040d
mkmahmud <mahmudulhasanmk434@gmail.com>
2022-10-11 14:48:34 +0600

    show correct ans

--- src/components/Home/SingelQuiz/Question/Question.js
+++ src/components/Home/SingelQuiz/Question/Question.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { EyeIcon } from '@heroicons/react/24/solid'
 import './Quistion.css'
 
 const Question = ({data}) => {
@@ -6,20 +7,30 @@ const Question = ({data}) => {
     console.log(correctAnswer)
 
     const [Qresult, setResult] = useState(' ');
+    const [click, setClick] = useState(false)
+    const [correctAns, setCorrectAns] = useState(false)
+    const [showCorrect, setShowCorrect] = useState(false)
     
     const result = (e) => {
+       
         if(e.target.innerText === correctAnswer){
           setResult('True')
         }else{
           setResult('False')
+          setCorrectAns(true)
         }
+        setClick(true)
     }
 
+    const showCorrectAns = () => {
+        setShowCorrect(true)
+    }
 
     return (
         <div className='question'>
             <h2 id='question'><span>01</span> {question} </h2>
-            <div className="options">
+            <p>{   <EyeIcon onClick={showCorrectAns}  className="eyeIcon"/>} </p>
+            <div className={`options ${click ? 'disabledbutton' : ' '}`}>
                 <ul>
                     {
                         options.map(opt => <li onClick={result}>{opt}</li> )
@@ -28,7 +39,9 @@ const Question = ({data}) => {
                 </ul>
             </div>
             <div className="answer">
-                
+            
+                <h4 className={`correctAns ${showCorrect ? 'displayblock' : 'displayNone'}`}><span>Correct Answer:</span> {correctAns ? correctAnswer : ' '}</h4>
+            
             <h2 className={`my ${Qresult}`}> {Qresult}</h2>
             </div>
         </div>
